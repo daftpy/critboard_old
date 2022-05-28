a7e078015bbbda70962a8ed80c92cdd2e494d6ee
Alex <daftpy@gmail.com>
2022-05-27 22:29:50 -0500

    Fixing more style bugs.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next'
-import { ReactElement } from 'react';
 import styles from '../styles/Home.module.css';
 import BaseLayout from '../components/baselayout';
 
@@ -11,7 +10,7 @@ const Home: NextPage = () => {
     <BaseLayout>
       <div className={`flex flex-col ${styles.wrapper}`}>
         <div><WelcomeBox /></div>
-        <div><HottestSubmissions /></div>
+        <div className='grow'><HottestSubmissions /></div>
       </div>
     </BaseLayout>
   )
